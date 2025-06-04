a6ae63c7c5e737305b260965cfc1dd98588fb98e
dolores3 <dolores@atom.nl>
2025-06-04 13:44:23 +0200

    removed apiurl in code

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,11 +5,12 @@ import { initializeAuth, getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
+const apiUrl = process.env.EXPO_PUBLIC_FIREBASE_API_URL;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzf8t9t_yd0hPasNaS_htdlHFEwsciVio",
+  apiKey: apiUrl,
   authDomain: "ple-dolores.firebaseapp.com",
   databaseURL: "https://ple-dolores-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "ple-dolores",
@@ -18,6 +19,8 @@ const firebaseConfig = {
   appId: "1:537574606878:web:ea1804b96e0b7b5230460f",
   measurementId: "G-GYG4J2GN4H"
 };
+
+console.log();
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
